d21d285d30c409e26834484a741876e0024153e3
Moritz Grosch <moritz.grosch@autinity.de>
2018-08-10 11:38:29 +0200

    Initialize settings from environment variables

--- init.rb
+++ init.rb
@@ -3,16 +3,16 @@ require_dependency 'redmine_auth_gitlab/hooks'
 
 Redmine::Plugin.register :redmine_auth_gitlab do
   name 'GitLab Authentication'
-  author 'Dmitry Kovalenok, Pete Deffendol'
+  author 'Moritz Grosch, Dmitry Kovalenok, Pete Deffendol'
   description 'This is a plugin for authentication through GitLab'
   version '0.0.1'
   url 'https://github.com/pdeffendol/redmine_auth_gitlab'
   author_url 'https://github.com/pdeffendol'
 
   settings :default => {
-    :client_id => "",
-    :client_secret => "",
-    :oauth_autentification => false,
-    :allowed_domains => ""
+    :client_id => ENV['REDMINE_GITLAB_CLIENT_ID'] || "",
+    :client_secret => ENV['REDMINE_GITLAB_CLIENT_SECRET'] || "",
+    :oauth_autentification => ENV['REDMINE_GITLAB_AUTH_ENABLED'] || false,
+    :allowed_domains => ENV['REDMINE_GITLAB_ALLOWED_DOMAINS'] || ""
   }, :partial => 'settings/gitlab_settings'
 end
